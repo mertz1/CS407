cd3da54ac54aa96a5d05afd6ddc359bfec650d5b
zhan1803 <zhan1803@purdue.edu>
2017-10-18 12:15:29 -0400

    third party application - finished validation

---
+++ CivilMapTestApplication/CheckAuthorization.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CivilMapTestApplication
+{
+    class CheckAuthorization
+    {
+        public bool CheckAccessAuthorization(string key)
+        {
+            bool hasAuthorization = false;
+            string pin = "ef258503-eb0f-4c4b-b265-99762f5d";
+
+            hasAuthorization = pin.Equals(key) ? true : false;
+            return hasAuthorization;
+        }
+    }
+}
---
+++ CivilMapTestApplication/TestApplication.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CivilMapSQLDatabase;
+
+namespace CivilMapTestApplication
+{
+    partial class TestApplication
+    {
+        static void Main(string[] args)
+        {
+            CheckAuthorization checkAuthorization = new CheckAuthorization();
+            string key = "ef258503-eb0f-4c3b-b265-99762f5d";
+
+            bool hasAuthorization = checkAuthorization.CheckAccessAuthorization(key);
+
+            if (hasAuthorization)
+            {
+                Debug.WriteLine("Has Authorization");
+            }
+            else
+            {
+                Debug.WriteLine("No Authorization");
+            }
+        }
+    }
+}
