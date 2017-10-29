2f222c3ed43929ea4e7816b73254d112745be702
Jing Zheng <zheng276@purdue.edu>
2017-10-29 15:04:03 -0500

    Upload APIs services for front end
    - Completed service for request by center and radius

---
+++ CivilMapServices/CivilMapDetails.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace CivilMapServices
+{
+    /* -------------------------------Request Model------------------------------ */
+    [DataContractAttribute()]
+    public class UserData
+    {
+        [DataMemberAttribute()]
+        public string username;
+        [DataMemberAttribute()]
+        public string password;
+        [DataMemberAttribute()]
+        public string token;
+    }
+
+
+    [DataContractAttribute()]
+    public class RequestPoint
+    {
+        [DataMemberAttribute()]
+        public string type;
+        [DataMemberAttribute()]
+        public string date_range;
+        [DataMemberAttribute()]
+        public string date_start;
+        [DataMemberAttribute()]
+        public string date_end;
+        [DataMemberAttribute()]
+        public string beat;
+    }
+
+    [DataContractAttribute()]
+    public class RequestPointByRadius
+    {
+        [DataMemberAttribute()]
+        public string type;
+        [DataMemberAttribute()]
+        public string date_range;
+        [DataMemberAttribute()]
+        public string date_start;
+        [DataMemberAttribute()]
+        public string date_end;
+        [DataMemberAttribute()]
+        public double radius;
+        [DataMemberAttribute()]
+        public double center_longitude;
+        [DataMemberAttribute()]
+        public double center_latitude;
+    }
+    /* ------------------------------Response Model------------------------------ */
+    public class TokenValidationStatus
+    {     
+        public bool Expired { get; set; }
+        public bool WrongUser { get; set; }
+    }
+
+    public class PointSet {
+        public PointSet() {
+            this.Points = new List<Point>();
+        }
+        public List<Point> Points { get; set; }
+        public string error;
+        public int count { get; set; }
+    }
+
+    public class Point {    
+        public string longitude { get; set; }
+        public string latitude { get; set; }
+        public string date { get; set; }
+        public double distance { get; set; }
+    }
+}
---
+++ CivilMapServices/ICivilMapService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Web;
+
+
+namespace CivilMapServices
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ICivilMapService" in both code and config file together.
+    [ServiceContract]
+    public interface ICivilMapService
+    {
+        /*  ------------------------------Authentication------------------------------ */
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+        ResponseFormat = WebMessageFormat.Json,
+        RequestFormat = WebMessageFormat.Json,
+        BodyStyle = WebMessageBodyStyle.Bare)]
+        string Login(UserData user);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+        ResponseFormat = WebMessageFormat.Json,
+        RequestFormat = WebMessageFormat.Json,
+        BodyStyle = WebMessageBodyStyle.WrappedRequest)]
+        void CreateUser(string username, string password);
+
+        /*------------------------------Data Request/Reponse------------------------------ */
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+        ResponseFormat = WebMessageFormat.Json,
+        RequestFormat = WebMessageFormat.Json,
+        BodyStyle = WebMessageBodyStyle.Bare)]
+        PointSet RequestPoints(RequestPoint Request);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+        ResponseFormat = WebMessageFormat.Json,
+        RequestFormat = WebMessageFormat.Json,
+        BodyStyle = WebMessageBodyStyle.Bare)]
+        PointSet RequestPointsByRadius(RequestPointByRadius RequestbyRadius);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+        ResponseFormat = WebMessageFormat.Json,
+        RequestFormat = WebMessageFormat.Json,
+        BodyStyle = WebMessageBodyStyle.WrappedRequest)]
+        List<string> getBeats(string type);
+    }
+}
