6cd57324dd96a56e68fc50ea7463f2bed668ebac
zhan1803 <zhan1803@purdue.edu>
2017-12-05 22:07:27 -0500

    Add files via upload

---
+++ CivilMapRESTApi/CivilMapRESTApi/Models/PurifiedAddressRESTModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+
+namespace CivilMapRESTApi.Models
+{
+    public class PurifiedAddressRESTModel
+    {
+        public string PurifiedAddressId { get; set; }
+        public AddressRESTModel AddressRESTModel { get; set; }
+        public Decimal Longitude { get; set; }
+        public Decimal Latitude { get; set; }
+    }
+}
