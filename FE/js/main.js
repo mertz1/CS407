67d8b3f07062fdc42df6754b841784cff22da9fe
Jing Zheng <zheng276@purdue.edu>
2018-01-08 13:17:27 -0500

    Latest Version JS

--- FE/js/main.js
+++ FE/js/main.js
@@ -45,6 +45,32 @@ $(document).ready(function(){
             pre_slide = false;
         }
     });
+    $("#p1").click(function(){
+        $("#pre_map_panel").slideUp("fast");
+        document.getElementById("pre_map").innerHTML = "Past Week";
+        pre_slide = false;
+        predefinedmapplot(0);
+    });
+    $("#p2").click(function(){
+        $("#pre_map_panel").slideUp("fast");
+        document.getElementById("pre_map").innerHTML = "Past Year";
+        pre_slide = false;
+        predefinedmapplot(1);
+    });
+    $("#p3").click(function(){
+        $("#pre_map_panel").slideUp("fast");
+        document.getElementById("pre_map").innerHTML = "This Year";
+        pre_slide = false;
+        predefinedmapplot(2);
+    });
+    $("#p4").click(function(){
+        $("#pre_map_panel").slideUp("fast");
+        document.getElementById("pre_map").innerHTML = "Past 2 Years";
+        pre_slide = false;
+        predefinedmapplot(3);
+    });
+
+
 
     var c_slide = false;
     form.crime_type.value = "crime";
@@ -186,6 +212,7 @@ function getBeat() {
 
 
 function submitForm() {
+    exportButton.disabled = false;
     resetGlobalVars();
 
     var form = document.getElementById("form").elements;
@@ -394,6 +421,7 @@ function plotByZoom(long3, lat3, radius3) {
 
     resetRequest();
 }
+
 
 function submitRadiusAndCenter() {
     var longitude = document.getElementsByName("longitude")[0].value;
@@ -617,4 +645,389 @@ function resetRequest() {
         'type': "",
         'beat': ""
     };
+}
+
+function inputMapID() {
+    var id_sec = document.getElementById("input_id_sec");
+    id_sec.innerHTML = "";
+    var html = '';
+    html =  '<input id="input_id_id" type="text" value=""></input>'+
+            '<button class="input_id_submit" onclick="plotMapFromSectionID()">submit</button>';
+    $('#input_id_sec').append(html);
+}
+
+var showP = false;
+function showPolygon() {
+    if(!showP) {
+        document.getElementById("polygon_sec").style.display = "block";
+        showP = true;
+    }
+    else { 
+        document.getElementById("polygon_sec").style.display = "none";
+        showP = false;
+    }
+}
+
+var showM = false;
+function showMapInput() {
+    if(!showM) {
+        document.getElementById("map_input_sec").style.display = "block";
+        showM = true;
+    }
+    else { 
+        document.getElementById("map_input_sec").style.display = "none";
+        showM = false;
+    }
+}
+
+
+function showPolygonOption() {
+    console.log("sth");
+    var type = document.getElementById("sel_type").value;
+    var area = document.getElementById("sel_area");
+    var dist = document.getElementById("sel_dist");
+    var sect = document.getElementById("sel_sec");
+    var beat = document.getElementById("sel_beat");
+    console.log(type);
+    if (type === "area") {
+        area.style.background = "white";
+        area.disabled = false;
+        dist.style.background = "#989494";
+        dist.disabled = true;
+        sect.style.background = "#989494";
+        sect.disabled = true;
+        beat.style.background = "#989494";
+        beat.disabled = true;
+        getAreaNumber("getCpdAreas");
+    } else if (type === "district") {
+        dist.style.background = "white";
+        dist.disabled = false;
+        area.style.background = "white";
+        area.disabled = false;
+        sect.style.background = "#989494";
+        sect.disabled = true;
+        beat.style.background = "#989494";
+        beat.disabled = true;
+        getAreaNumber("getCpdAreas");
+        getDistNumber("getCpdDistricts");
+    } else if (type === "sector") {
+        area.style.background = "white";
+        area.disabled = false;
+        dist.style.background = "white";
+        dist.disabled = false;
+        sect.style.background = "white";
+        sect.disabled = false;
+        beat.style.background = "#989494";
+        beat.disabled = true;
+        getAreaNumber("getCpdAreas");
+        getDistNumber("getCpdDistricts");
+        getSecNumber("getCpdSectors");
+    } else if (type === "beat") {
+        area.style.background = "white";
+        area.disabled = false;
+        beat.style.background = "white";
+        beat.disabled = false;
+        dist.style.background = "white";
+        dist.disabled = false;
+        sect.style.background = "white";
+        sect.disabled = false;
+        getAreaNumber("getCpdAreas");
+        getDistNumber("getCpdDistricts");
+        getSecNumber("getCpdSectors");
+        getBeatNumber("getCpdBeats");
+    }
+}
+
+function showAreaNumber(num) {
+    console.log(num);
+    var html = '';
+    for (i = 0; i < num.length; i++)
+        html += '<option value="'+num[i]+'">'+num[i]+'</option>';
+    $('#sel_area').append(html);
+}
+function showDistNumber(num) {
+    console.log(num);
+    var html = '';
+    for (i = 0; i < num.length; i++)
+        html += '<option value="'+num[i]+'">'+num[i]+'</option>';
+    $('#sel_dist').append(html);
+}
+function showSecNumber(num) {
+    console.log(num);
+    var html = '';
+    for (i = 0; i < num.length; i++)
+        html += '<option value="'+num[i]+'">'+num[i]+'</option>';
+    $('#sel_sec').append(html);
+}
+function showBeatNumber(num) {
+    console.log(num);
+    var html = '';
+    for (i = 0; i < num.length; i++)
+        html += '<option value="'+num[i]+'">'+num[i]+'</option>';
+    $('#sel_beat').append(html);
+}
+
+function getAreaNumber(m) {
+    $.ajax({
+        url: service + m,
+        method: 'POST',
+        contentType: 'application/json; charset=utf-8',
+        data: "",
+        dataType: 'json',
+        success: function(data) {
+            if (!(data.includes("error"))) {
+                //console.log(data);
+                showAreaNumber(data);
+            } else {
+                console.log("error");
+                console.log(data);
+            }
+        }
+    });
+}
+function getDistNumber(m, opt) {
+    var poly = document.getElementById("polygon_sec");
+    $.ajax({
+        url: service + m,
+        method: 'POST',
+        contentType: 'application/json; charset=utf-8',
+        data: "",
+        dataType: 'json',
+        success: function(data) {
+            if (!(data.includes("error"))) {
+                showDistNumber(data);
+            } else {
+                console.log("error");
+                console.log(data);
+            }
+        }
+    });
+}
+function getSecNumber(m) {
+    $.ajax({
+        url: service + m,
+        method: 'POST',
+        contentType: 'application/json; charset=utf-8',
+        data: "",
+        dataType: 'json',
+        success: function(data) {
+            if (!(data.includes("error"))) {
+                showSecNumber(data);
+            } else {
+                console.log("error");
+                console.log(data);
+            }
+        }
+    });
+}
+function getBeatNumber(m) {
+    $.ajax({
+        url: service + m,
+        method: 'POST',
+        contentType: 'application/json; charset=utf-8',
+        data: "",
+        dataType: 'json',
+        success: function(data) {
+            if (!(data.includes("error"))) {
+                showBeatNumber(data);
+            } else {
+                console.log("error");
+                console.log(data);
+            }
+        }
+    });
+}
+    ifIsPoly = 0; // area
+    ifIsDistrict = 0; // district 
+    ifIsBeat = 0;
+    ifIsSector = 0;
+    var poly;
+
+function drawPolygon() {
+    resetGlobalVars();
+    document.getElementById("polygon_sec").style.display = "none";
+
+    var type = document.getElementById("sel_type").value;
+    var area = document.getElementById("sel_area").value;
+    var dist = document.getElementById("sel_dist").value;
+    var sect = document.getElementById("sel_sec").value;
+    var beat = document.getElementById("sel_beat").value;
+    var ac_type = document.getElementById("form").elements.crime_type.value;
+
+    var polygon = {"option":"","area":"","district":"","sector":"","type":"","beat":""};
+    polygon["option"] = type;
+    polygon["area"] = area;
+    polygon["district"] = dist;
+    polygon["sector"] = sect;
+    polygon["type"] = ac_type;
+    polygon["beat"] = beat;
+    console.log(polygon);
+    
+    //
+    var request2 = polygon["type"];
+    var points = {};
+    $.ajax({
+        url: service + "GetPointsForPolygon",
+        method: 'POST',
+        contentType: 'application/json; charset=utf-8',
+        data: JSON.stringify(polygon),
+        dataType: 'json',
+        success: function(data) {
+            $.each(data["Points"], function(i) {
+                var point = data["Points"][i];
+                points[i] = { longitude: point["longitude"], latitude: point["latitude"], date: point["date"] };
+            });
+            points["count"] = data["count"];
+            //call your function for map render as a callback function of succeed action, example:
+            plotmap(points, request2);
+            console.log(points);
+            getAllFeatures();
+        },
+        error: function(data) {
+            if (data.d["error"] !== 'undefined') {
+                alert("Error: " + JSON.stringify(data.d["error"]));
+            } else {
+                alert("Unhandled Error: " + JSON.stringify(data));
+            }
+        }
+    });
+    console.log(points);
+    //
+    if (polygon["option"] == "area") {
+        areaSource.forEachFeature(function(feature) {
+            if (feature.get('AREA_NUM') == polygon["area"]) {
+                //console.log("It should go in");
+                var tempSource = new ol.source.Vector({
+                    features: feature
+                });
+                console.log(feature);
+                poly = feature.get('geometry');
+                console.log(poly);
+                console.log(poly.getExtent());
+                poly = poly.getExtent();
+                //var polytest = feature[0].get('geometry');
+                //console.log(polytest);
+                ifIsPoly = 1;
+                tempSource.addFeature(feature);
+                var areas = new ol.layer.Vector({
+                    source: tempSource,
+                    style: new ol.style.Style({
+                        stroke: new ol.style.Stroke({
+                            color: '#319FD3',
+                            width: 3
+                        })
+                    })
+                })
+                a = areas;
+                 //areas.setZIndex(2);
+                map.getLayers().insertAt(2, areas);
+                areas.setVisible(true);
+               
+                //areas.setActive(false);
+            }
+    
+        });
+    } else if (polygon["option"] == "district") {
+        districtSource.forEachFeature(function(feature){
+           if (feature.get('DISTRICT') == polygon["district"]) {
+                var tempSource = new ol.source.Vector({
+                    features: feature
+                });
+                ifIsDistrict = 1;
+                tempSource.addFeature(feature);
+                var districts = new ol.layer.Vector({
+                    source: tempSource,
+                    style: new ol.style.Style({
+                        stroke: new ol.style.Stroke({
+                            color: '#FFA500',
+                            width: 3
+                        })
+                    })
+                })
+                d = districts;
+                //map.addLayer(districts);
+                map.getLayers().insertAt(3, districts);
+                districts.setVisible(true);
+           }
+        });
+                areaSource.forEachFeature(function(feature) {
+            if (feature.get('AREA_NUM') == polygon["area"]) {
+                //console.log("It should go in");
+                var tempSource = new ol.source.Vector({
+                    features: feature
+                });
+                console.log(feature);
+                poly = feature.get('geometry');
+                console.log(poly);
+                console.log(poly.getExtent());
+                poly = poly.getExtent();
+                //var polytest = feature[0].get('geometry');
+                //console.log(polytest);
+                ifIsPoly = 1;
+                tempSource.addFeature(feature);
+                var areas = new ol.layer.Vector({
+                    source: tempSource,
+                    style: new ol.style.Style({
+                        stroke: new ol.style.Stroke({
+                            color: '#319FD3',
+                            width: 7
+                        })
+                    })
+                })
+                a = areas;
+                 //areas.setZIndex(2);
+                map.getLayers().insertAt(3, areas);
+                areas.setVisible(true);
+               
+                //areas.setActive(false);
+            }
+    
+        });
+    } else if (polygon["option"] == "sector") { // sector id and district name
+        sectorSource.forEachFeature(function(feature){
+            console.log(feature.get('SECTOR'));
+           if (feature.get('SECTOR') == polygon["sector"]) {
+                var tempSource = new ol.source.Vector({
+                    features: feature
+                });
+                ifIsSector = 1;
+                tempSource.addFeature(feature);
+                var sectors = new ol.layer.Vector({
+                    source: tempSource,
+                    style: new ol.style.Style({
+                        stroke: new ol.style.Stroke({
+                            color: '#FF0000',
+                            width: 3
+                        })
+                    })
+                })
+                s = sectors;
+                //map.addLayer(sectors);
+                map.getLayers().insertAt(2, sectors);
+                sectors.setVisible(true);
+           }
+        });
+    } else if (polygon["option"] == "beat") { // beat, district and sector
+        beatsSource.forEachFeature(function(feature){
+           if (feature.get('BEAT') == polygon["beat"]) {
+                var tempSource = new ol.source.Vector({
+                    features: feature
+                });
+                ifIsBeat = 1;
+                tempSource.addFeature(feature);
+                var beats = new ol.layer.Vector({
+                    source: tempSource,
+                    style: new ol.style.Style({
+                        stroke: new ol.style.Stroke({
+                            color: '#00FF00',
+                            width: 3
+                        })
+                    })
+                })
+                b = beats;
+                map.addLayer(beats);
+                beats.setVisible(true);
+           }
+        });
+    }
 }
